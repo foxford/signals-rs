82cea20f3c33815c0d54e26585c040ab5a6ad286
Aleksey Ivanov <ialexxei@gmail.com>
2018-02-11 00:00:18 +0300

    Fetch MQTT options from ENV

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 extern crate rumqtt;
 extern crate signals;
 
+use std::{env, process};
+
 fn main() {
+    let mqtt_client_id = match env::var("MQTT_CLIENT_ID") {
+        Ok(val) => val,
+        Err(err) => {
+            println!("MQTT_CLIENT_ID {}", err);
+            process::exit(1);
+        }
+    };
+
     let mqtt_options = rumqtt::MqttOptions::new()
         .set_keep_alive(5)
         .set_reconnect(3)
-        .set_client_id("signals-rs");
+        .set_client_id(mqtt_client_id);
 
     signals::run(mqtt_options);
 }
