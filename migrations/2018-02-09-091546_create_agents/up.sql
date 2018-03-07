17e9154327a7eba3bf9a0d0d4b78ee3419c3a907
Aleksey Ivanov <ialexxei@gmail.com>
2018-03-07 16:47:10 +0300

    Add ON DELETE CASCADE for agents.room_id

--- migrations/2018-02-09-091546_create_agents/up.sql
+++ migrations/2018-02-09-091546_create_agents/up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE agents (
   id uuid PRIMARY KEY NOT NULL,
   label TEXT NOT NULL DEFAULT '',
-  room_id uuid NOT NULL REFERENCES rooms (id)
+  room_id uuid NOT NULL REFERENCES rooms (id) ON DELETE CASCADE
 )
